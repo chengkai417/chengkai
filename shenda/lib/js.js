005a6365f64a5a08af7c39de1adb5fa7cc6860ed
chengkai <2452587090@qq.com>
2017-12-12 20:30:19 +0800

    first commit

---
+++ shenda/lib/js.js
@@ -0,0 +1,7 @@
+﻿var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?eaa22075ffedfde4dc734cdbc709273d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
